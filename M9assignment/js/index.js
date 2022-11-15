3a04c53efdd033c7ec50d8f284fd02eedd93896e
WendyChen2022 <114187471+WendyChen2022@users.noreply.github.com>
2022-11-14 20:47:30 -0800

    Add files via upload

---
+++ M9assignment/js/index.js
@@ -0,0 +1,35 @@
+
+var mybutton = document.querySelector("button");
+mybutton.addEventListener("click", function(event) {
+    var element = document.getElementsByTagName("div");
+    for (index = element.length - 1; index >= 0; index--) {
+        element[index].parentNode.removeChild(element[index]);
+    }
+
+    // Let us stop the propagation of events
+
+    event.stopPropagation();
+});
+
+addEventListener("click",function(event) {
+    var dot = document.createElement("div");
+    var size = document.getElementById("size");
+    var color = document.getElementById("colorSelected");
+    console.log(size.value);
+    console.log(color.value);
+    dot.className = "dot";
+    
+    dot.style.left = (event.pageX - 4) + "px";
+    dot.style.top = (event.pageY - 4) + "px";
+
+
+    document.body.appendChild(dot);
+});
+
+
+
+
+
+
+
+
