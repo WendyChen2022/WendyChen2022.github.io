0c57c6b7df4ce4fa9c09572f317b4b694dcf83a8
WendyChen2022 <114187471+WendyChen2022@users.noreply.github.com>
2022-11-15 11:16:03 -0800

    Update index.js

--- M9assignment/js/index.js
+++ M9assignment/js/index.js
@@ -1,5 +1,8 @@
 var mybutton = document.querySelector("button");
-mybutton.addEventListener("click", function(event) {
+var size = document.getElementById("size");
+var color = document.getElementById("colorSelected");
+
+mybutton.addEventListener("click", function (event) {
     var element = document.getElementsByTagName("div");
     for (index = element.length - 1; index >= 0; index--) {
         element[index].parentNode.removeChild(element[index]);
@@ -10,20 +13,25 @@ mybutton.addEventListener("click", function(event) {
     event.stopPropagation();
 });
 
-addEventListener("click",function(event) {
+addEventListener("click", addDot);
+
+
+function addDot(event) {
     var dot = document.createElement("div");
-    var size = document.getElementById("size");
-    var color = document.getElementById("colorSelected");
-    console.log(size.value);
-    console.log(color.value);
-    dot.className = "dot";
-    
-    dot.style.left = (event.pageX - 4) + "px";
-    dot.style.top = (event.pageY - 4) + "px";
 
+    if (event.pageX<78 && event.pageY<78) {
+        dot.className = "";
+    } else {
+        dot.className = "dot";
+        dot.style.left = (event.pageX - 4) + "px";
+        dot.style.top = (event.pageY - 4) + "px";
+        dot.style.borderColor = color.value;
+        dot.style.borderWidth = size.value + "px";
 
-    document.body.appendChild(dot);
-});
+        document.body.appendChild(dot);
+    }
+
+}
 
 
 
